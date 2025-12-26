f25408dbf5128ea74135d9c4bbde29acb1b90b7c
Hontans <163842666+Hontans@users.noreply.github.com>
2025-12-27 00:03:31 +0100

    feat: add basic todolist functionality with task management
    - Implemented a todolist component with the ability to add and remove tasks.
    - Added input field for new tasks and a button to submit.
    - Displayed tasks in a list format with a validation button for each task.
    - Introduced TypeScript interfaces for task structure.

---
+++ src/Todolist.vue
@@ -0,0 +1,41 @@
+<template>
+<h1>Todolist</h1>
+<h2 v-if="todolist.length === 0">Tu n'as toujours pas de tâche à faire ?</h2>
+<h2 v-else>Tu doit acomplire ces tache !</h2>
+
+<form action="" @submit.prevent="addTask">
+    <input type="text" v-model="newTask">
+    <button> ajouter une tache</button>
+</form>
+
+<ul>
+    <li v-for="task in todolist">{{ task.titel }} <button @click="remouveTask(task)">valider</button></li>
+</ul>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    interface Task {
+        titel: string;
+        completed: boolean;
+        date: Date;
+    }
+
+    const todolist = ref<Task[]>([])
+
+    const newTask = ref('')
+
+    const addTask = () => {
+        todolist.value.push({
+            titel: newTask.value,
+            completed: false,
+            date: new Date()
+        })
+        newTask.value = ''
+    }
+
+    const remouveTask = (task : Task) => {
+        todolist.value = todolist.value.filter(tasks => tasks !== task)
+    }
+</script>
