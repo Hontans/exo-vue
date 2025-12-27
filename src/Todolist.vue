bb5200cd40488844bfecc89c83a2db8c0ca67f9e
Hontans <163842666+Hontans@users.noreply.github.com>
2025-12-27 18:05:38 +0100

    fix: corriger l'affichage des tâches et améliorer la gestion des tâches complètes

--- src/Todolist.vue
+++ src/Todolist.vue
@@ -1,18 +1,26 @@
 <template>
-<h1>Todolist</h1>
-<h2 v-if="todolist.length === 0">Tu n'as toujours pas de tâche à faire ?</h2>
-<h2 v-else>Tu doit acomplire ces tache !</h2>
+    <h1>Todolist</h1>
+    <h2 v-if="todolist.length === 0">Tu n'as toujours pas de tâche à faire ?</h2>
+    <h2 v-else>Tu doit acomplire ces tache !</h2>
 
-<form action="" @submit.prevent="addTask">
-    <input type="text" v-model="newTask">
-    <button> ajouter une tache</button>
-</form>
+    <form action="" @submit.prevent="addTask">
+        <input type="text" v-model="newTask">
+        <button> ajouter une tache</button>
+    </form>
 
-<ul>
-    <li v-for="task in todolist" :key="task.titel">
-        <input type="checkbox" v-if="task.completed == false" @click="remouveTask(task)"> {{ task.titel }}
-    </li>
-</ul>
+    <ul>
+        <li v-for="task in todolist" :key="task.titel">
+            <input type="checkbox" v-if="task.completed == false" @click="remouveTask(task)"> {{ task.titel }}
+        </li>
+    </ul>
+
+
+    <button @click="showCompleted = !showCompleted">Afficher les tâches accomplies</button>
+    <ul v-if="showCompleted">    
+        <li v-for="task in taskEnded" :key="task.titel">
+            <input type="checkbox" checked disabled> {{ task.titel }}
+        </li>
+    </ul>
 </template>
 
 <script setup lang="ts">
@@ -26,6 +34,10 @@
 
     const todolist = ref<Task[]>([])
 
+    const taskEnded = ref<Task[]>([])
+
+    const showCompleted = ref(false)
+
     const newTask = ref('')
 
     const addTask = () => {
@@ -38,6 +50,8 @@
     }
 
     const remouveTask = (task : Task) => {
+        task.completed = true
         todolist.value = todolist.value.filter(tasks => tasks !== task)
+        taskEnded.value.push(task)
     }
 </script>
