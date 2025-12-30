9de891ce2832303461cc53df33417464201eb723
Hontans <163842666+Hontans@users.noreply.github.com>
2025-12-30 01:08:26 +0100

    feat: ajouter une navigation entre le questionnaire et la liste de tâches

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,7 +1,26 @@
 <template>
-
+    <nav>
+        <ul>
+            <li><a href="./Todolist.vue">Todolist</a></li>
+            <li><a href="./Quiz.vue">Quiz</a></li>
+        </ul>
+    </nav>
+    {{ quiz }}
 </template>
 
 <script setup lang="ts">
+    import { onMounted, ref } from 'vue';
 
+    const quiz = ref(null)
+
+    onMounted(()=> {
+        fetch ('../public/quiz.json')
+            .then(r => {
+                if (r.ok) {
+                    return r.json()
+                }
+                
+                throw new Error("le fichier na pas été charger")
+            })
+    })
 </script>
